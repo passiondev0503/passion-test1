8003bfbbd1152070b4404538074db87be0329765
passiondev0503 <passiondev0503@gmail.com>
2023-11-20 13:02:11 -0500

    separate actions

--- src/actions/userActions.tsx
+++ src/actions/userActions.tsx
@@ -45,17 +45,3 @@ export const register = (
     .then((res) => {
       return setRegisterSuccess(true)
     })
-
-export const GetAllProducts = async () => {
-  const response = await axios.get(`${API_URL}/products`)
-  return response.data
-}
-
-export const GetSingleProduct = (id: number) =>
-  axios.get(`${API_URL}/products/${id}`).then((res) => res.data)
-
-export const GetAllCategories = () =>
-  axios.get(`${API_URL}/products/categories`).then((res) => res.data)
-
-export const GetInCategory = (name: string) =>
-  axios.get(`${API_URL}/products/category/${name}`).then((res) => res.data)
