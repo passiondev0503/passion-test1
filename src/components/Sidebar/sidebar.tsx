7951cc544bd7084e906d94cf0e4b8c293ef8a0b3
passiondev0503 <passiondev0503@gmail.com>
2023-11-20 00:10:04 -0500

    profile page and sidebar work

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,26 +1,136 @@
+import React, { useState } from "react"
 import "./style.css"
 
 const SideBar = () => {
+  const [activeIndex, setActiveIndex] = useState(-1)
+  const [subMenu, setSubMenu] = useState(false)
+
+  function handleClick(index: any) {
+    setActiveIndex(activeIndex === index ? -1 : index)
+  }
+
+  const handleMenu = () => {
+    console.log("123123")
+    setSubMenu(!subMenu)
+  }
+
   return (
-    <div className="sidebar">
-      <div className="sidebar_header">
-        <img alt="logo" src="/logo.jpg" />
-        <p>TEST</p>
+    <>
+      {/* <div className="sidebar">
+        <div className="sidebar_header">
+          <img alt="logo" src="/logo.jpg" />
+          <p>TEST</p>
+        </div>
+
+
+        <div className="sidebar_footer">
+          <img alt="photo" className="photo" src="/photo.jpg" />
+          <p className="name">Jacob Kevin</p>
+        </div>
+      </div> */}
+      {/* <div id="side-bar">
+        <input id="side-toggle" type="checkbox" />
+        <div id="side-header">
+          <a id="side-title" href="https://codepen.io" target="_blank">
+            C<i className="fab fa-codepen"></i>DEPEN
+          </a>
+          <label htmlFor="side-toggle">
+            <span id="side-toggle-burger"></span>
+          </label>
+          <hr />
+        </div>
+        <div id="side-content">
+          <div className="side-button">
+            <i className="fas fa-palette"></i>
+            <span>Dashboard</span>
+          </div>
+          <hr />
+          <div className="side-button">
+            <i className="fas fa-heart"></i>
+            <span>Product</span>
+          </div>
+          <hr />
+          <div className="side-button">
+            <i className="fas fa-gem"></i>
+            <span>Profile</span>
+          </div>
+          <div id="side-content-highlight"></div>
+        </div>
+        <input id="side-footer-toggle" type="checkbox" />
+        <div id="side-footer">
+          <div id="side-footer-heading">
+            <div id="side-footer-avatar">
+              <img src="/photo.jpg" />
+            </div>
+            <div id="side-footer-titlebox">
+              <a
+                id="side-footer-title"
+                href="https://codepen.io/uahnbu/pens/public"
+                target="_blank"
+              >
+                uahnbu
+              </a>
+              <span id="side-footer-subtitle">Admin</span>
+            </div>
+            <label htmlFor="side-footer-toggle">
+              <i className="fas fa-caret-up"></i>
+            </label>
+          </div>
+        </div>
+      </div> */}
+      <div className="layout has-sidebar fixed-sidebar fixed-header">
+        <aside id="sidebar" className="sidebar break-point-sm has-bg-image">
+          <a id="btn-collapse" className="sidebar-collapser">
+            <i className="ri-arrow-left-s-line"></i>
+          </a>
+          <div className="sidebar-layout">
+            <div className="sidebar-header">
+              <div className="pro-sidebar-logo">
+                <div>P</div>
+                <h5>Pro Sidebar</h5>
+              </div>
+            </div>
+            <div className="sidebar-content">
+              <nav className="menu open-current-submenu">
+                <ul>
+                  <li className="menu-header">
+                    <span> GENERAL </span>
+                  </li>
+                  <li className="menu-item sub-menu">
+                    <a onClick={handleMenu}>
+                      <span className="menu-title">Components</span>
+                    </a>
+                    {subMenu ? (
+                      <div className="sub-menu-list">
+                        <ul>
+                          <li className="menu-item">
+                            <a href="#">
+                              <span className="menu-title">Grid</span>
+                            </a>
+                          </li>
+                          <li className="menu-item">
+                            <a href="#">
+                              <span className="menu-title">Layout</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                  </li>
+                  <li className="menu-item">
+                    <a href="/profile">
+                      <span className="menu-title">Profile</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </aside>
       </div>
-      <div className="menu">
-        <ul>
-          <li>home</li>
-          <li>home</li>
-          <li>home</li>
-          <li>home</li>
-          <li>home</li>
-        </ul>
-      </div>
-      <div className="sidebar_footer">
-        <img alt="photo" className="photo" src="/photo.jpg" />
-        <p className="name">Jacob Kevin</p>
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,65 @@
+import React, { useEffect, useState } from "react"
+
+import "./style.css"
+import Toast from "@/components/Notification"
+
+import { ToastContainer } from "react-toastify"
+
 const Profile = () => {
-  return <>Profile</>
+  const [tab, setTab] = useState("profile")
+  const handleTab = (list: string) => {
+    setTab(list)
+  }
+  const [showToast, setShowToast] = useState(false)
+
+  const notify = () => setShowToast(!showToast)
+  useEffect(() => {}, [showToast])
+  return (
+    <>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Toast message="Wow so easy!" showToast={true} />
+        <div className="card" style={{ padding: "10px" }}>
+          <div className="list" onClick={() => handleTab("profile")}>
+            Profile
+          </div>
+          <div className="list" onClick={() => handleTab("setting")}>
+            Setting
+          </div>
+        </div>
+        <div className="card" style={{ width: "100%" }}>
+          {tab === "profile" ? (
+            <>
+              <input type="file" />
+              <div className="inputContainer">
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" placeholder=" " />
+              </div>
+            </>
+          ) : (
+            <div style={{ padding: "10px 30px 30px 30px" }}>
+              <p>Change Password</p>
+              <div className="inputContainer">
+                <label htmlFor="old_pass">Old Password</label>
+                <input type="password" id="old_pass" placeholder="" />
+              </div>
+              <div className="inputContainer">
+                <label htmlFor="new_pass">New Password</label>
+                <input type="password" id="new_pass" placeholder="" />
+              </div>
+              <div className="inputContainer">
+                <label htmlFor="confirm_pass">Confirm Password</label>
+                <input type="password" id="confirm_pass" placeholder="" />
+              </div>
+              <div style={{ display: "flex", justifyContent: "end", gap: "10px" }}>
+                <button onClick={notify}>Notify!</button>
+                <button>Cancel</button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Profile
