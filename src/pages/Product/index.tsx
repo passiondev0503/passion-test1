d0a5d66905fa2c7367ad4e33151a428b90d2c5d1
passiondev0503 <passiondev0503@gmail.com>
2023-11-20 15:41:44 -0500

    start session 2: Actions (API integration)

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -4,19 +4,31 @@ import { useState, useEffect } from "react"
 import "./style.css"
 
 const Product = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState<any[]>([])
   const [categories, setCategories] = useState([])
+  const [search, setSearch] = useState<string>("")
 
   const handleCategory = async (data: string) => {
-    if (data != "") {
-      const list = await GetInCategory(data)
-      setProducts(list)
-    } else GetAllProducts(setProducts)
+    let list
+    if (data !== "") {
+      list = await GetInCategory(data)
+    } else {
+      list = await GetAllProducts()
+    }
+    setProducts(list)
   }
 
   useEffect(() => {
-    GetAllCategories(setCategories)
-    GetAllProducts(setProducts)
+    const handleAllCategory = async () => {
+      const list = await GetAllCategories()
+      setCategories(list)
+    }
+    const handleAllProducts = async () => {
+      const list: any[] = await GetAllProducts()
+      setProducts(list)
+    }
+    handleAllCategory()
+    handleAllProducts()
   }, [])
 
   return (
@@ -28,7 +40,14 @@ const Product = () => {
             style={{ display: "flex", alignItems: "center", gap: "4px" }}
           >
             <p style={{ fontSize: "18px" }}>Search:</p>
-            <input type="search" id="search" placeholder=" " className="product_input" />
+            <input
+              type="search"
+              id="search"
+              placeholder=" "
+              className="product_input"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
           </div>
           <div
             className="inputContainer"
@@ -50,26 +69,27 @@ const Product = () => {
         <div className="grid-container">
           {products &&
             products?.map((item, index) => {
-              return (
-                <div className="products" key={index}>
-                  <img src={item.image} alt={item?.title} className="product" />
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      fontWeight: "700",
-                      fontSize: "16px",
-                    }}
-                  >
-                    <p className="product-title">{item?.title}</p>
-                    <p>${item?.price}</p>
+              if (item?.title.includes(search) || item?.description.includes(search))
+                return (
+                  <div className="products" key={index}>
+                    <img src={item.image} alt={item?.title} className="product" />
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        fontWeight: "700",
+                        fontSize: "16px",
+                      }}
+                    >
+                      <p className="product-title">{item?.title}</p>
+                      <p>${item?.price}</p>
+                    </div>
+                    <p className="description">{item?.description}</p>
+                    <a href={`/product/${item.id}`}>
+                      <button style={{ width: "100%" }}>More Details</button>
+                    </a>
                   </div>
-                  <p className="description">{item?.description}</p>
-                  <a href={`/product/${item.id}`}>
-                    <button style={{ width: "100%" }}>More Details</button>
-                  </a>
-                </div>
-              )
+                )
             })}
         </div>
       </div>
